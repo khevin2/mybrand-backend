a48d2ea3ffd6130f59c96d125f5288f0e3f2994a
Cyusa Kheven <cyusa.khevin100@gmail.com>
2023-01-30 17:23:46 +0200

    chore(swagger): Add swagger documentation on routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,54 @@ const upload = multer()
 import { getAllUsers, addUser, getOneUser, deleteOneUser, updateUser } from './../controllers/users.controller.js'
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          properties:
+ *              _id:
+ *                  type: string
+ *              names:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              password:
+ *                  type: string
+ *              phone:
+ *                  type: string
+ *              dob:
+ *                  type: date
+ *              photo:
+ *                  type: string
+ *          required:
+ *              - names
+ *              - email
+ *              - password
+ *              - phone
+ *              - dob
+ *              - photo
+ *              
+ */
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      summary: Get a list of all users
+ *      responses:
+ *          '200':
+ *              description: All Users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/User'
+ *          '500': An error occured
+ */
+
 router.get('/', getAllUsers)
 router.post('/', upload.single('photo'), ValidateUser, addUser)
 router.get('/:id', getOneUser)
